127e4ac865fbf7eb09c46d5975bef441f2b20c0a
juanpi194 <juanpiviz194@gmail.com>
2026-07-02 17:17:14 +0200

    MAX ROOMS added

--- final/inc/world/World.hpp
+++ final/inc/world/World.hpp
@@ -11,6 +11,7 @@ class World
 		static constexpr size_t	MIN_NAME_LENGTH = 4;
 		static constexpr size_t	MAX_NAME_LENGTH = 40;
 		static constexpr bool	TITLE_NAME = true;
+		static constexpr size_t	MAX_ROOMS = 15;
 
 		/**
 		 * @brief	Checks that the provided name for the world
